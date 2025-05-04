42e66c77acb09340cf5819b04a7c14bde0df8a04
Miklos Gyori <61111659+miklosgyori@users.noreply.github.com>
2025-05-04 20:26:08 +0200

    KonyvPanel class improved: new function buttons

--- src/view/KonyvPanel.java
+++ src/view/KonyvPanel.java
@@ -16,6 +16,11 @@ public class KonyvPanel extends JPanel {
     private JTable table;
     private DefaultTableModel tableModel;
     private JButton loadButton;
+    private JButton addButton;
+    private JButton editButton;
+    private JButton deleteButton;
+    private JButton searchButton;
+    private JTextField searchField;
 
     public KonyvPanel() {
         setLayout(new BorderLayout());
@@ -30,13 +35,33 @@ public class KonyvPanel extends JPanel {
         table = new JTable(tableModel);
         JScrollPane scrollPane = new JScrollPane(table);
 
-        // betoltes gomb
+        // gombok
         loadButton = new JButton("Konyvek betoltese");
         loadButton.addActionListener(e -> loadBooks());
 
-        // top panel a gombbal
+        addButton = new JButton("Uj konyv rogzitese");
+        addButton.addActionListener(e -> addNewBook());
+
+        editButton = new JButton("Konyv adatainak szerkesztese");
+        editButton.addActionListener(e -> editSelectedBook());
+
+        deleteButton = new JButton("Konyv torlese");
+        deleteButton.addActionListener(e -> deleteSelectedBook());
+
+        searchButton = new JButton("Konyv keresese");
+        searchButton.addActionListener(e -> searchBooks());
+
+        searchField = new JTextField(20);
+
+
+        // top panel a gombokkal
         JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         topPanel.add(loadButton);
+        topPanel.add(addButton);
+        topPanel.add(editButton);
+        topPanel.add(deleteButton);
+        topPanel.add(searchField);
+        topPanel.add(searchButton);
 
         // komponensek hozzaadasa
         add(topPanel, BorderLayout.NORTH);
@@ -66,5 +91,21 @@ public class KonyvPanel extends JPanel {
                 konyvek.size() + " konyv betoltve.",
                 "A betoltes kesz",
                 JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private void addNewBook() {
+        JOptionPane.showMessageDialog(this, "Add book form not yet implemented.");
+    }
+
+    private void editSelectedBook() {
+        JOptionPane.showMessageDialog(this, "Edit book form not yet implemented.");
+    }
+
+    private void deleteSelectedBook() {
+        JOptionPane.showMessageDialog(this, "Delete functionality not yet implemented.");
+    }
+
+    private void searchBooks() {
+        JOptionPane.showMessageDialog(this, "Search not yet implemented.");
     }
 }
